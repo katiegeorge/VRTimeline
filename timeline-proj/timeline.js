9737e508762a1304a5effb377f44044d1a0b76b0
Katie George <katie.george@dhcp-10-105-128-192.wireless.northwestern.private>
2017-02-23 11:00:58 -0600

    fuse set to false

--- timeline-proj/timeline.js
+++ timeline-proj/timeline.js
@@ -6,24 +6,31 @@ var womens_march_button = document.querySelector("#womens_march_button")
 var sup_scene = document.querySelector("#supremecourtscene");
 var seneca = document.querySelector("#seneca");
 
+var womensmarchaudio = document.querySelector("#womensmarchaudio");
+var nineteenthamendmentaudio = document.querySelector("#nineteenthamendmentaudio");
+var senecafallsaudio = document.querySelector("#senecafallsaudio");
 
-var intro = document.querySelector("#intro");
 
-scene.addEventListener('click', function() {
-  intro.setAttribute('visible', 'false');
-  console.log('clicked');
-});
+//var intro = document.querySelector("#intro");
+
+// scene.addEventListener('click', function() {
+//   intro.setAttribute('visible', 'false');
+// });
 
 seneca_button.addEventListener('click', function() {
   showScene(seneca.id);
+  //playSceneSound(senecafallsaudio.id);
+  senecafallsaudio.components.sound.playSound();
 });
 
 sup_scene_button.addEventListener('click', function() {
   showScene(sup_scene.id);
+  playSceneSound(nineteenthamendmentaudio.id);
 });
 
 womens_march_button.addEventListener('click', function() {
   showScene(womens_march.id);
+  playSceneSound(womensmarchaudio.id);
 });
 
 
@@ -36,6 +43,15 @@ function showScene(id) {
   });
 
   document.querySelector("#" + id).setAttribute("visible", "true");
+}
+
+function playSceneSound(id) {
+    [...document.querySelectorAll('.sounds')].forEach( function(s) {
+    s.components.sound.pauseSound();
+  });
+
+  document.querySelector("#" + id).components.sound.playSound();
+
 }
 
 //this function is for when i get the click to work on individual dates in the timeline
