d83d0e65ff1f3e0171367174c66ad440b32f3ecc
Katie George <katie.george@dhcp-10-105-97-90.wireless.northwestern.private>
2017-03-07 13:54:05 -0600

    added in all scenes except anita hill, audio working and playing automatically when switching scenes its a miracle

--- timeline-proj/timeline.js
+++ timeline-proj/timeline.js
@@ -2,13 +2,23 @@ var scene = document.querySelector('a-scene');
 
 var seneca_button = document.querySelector("#seneca_button");
 var sup_scene_button = document.querySelector("#sup_scene_button");
-var womens_march_button = document.querySelector("#womens_march_button")
+var womens_march_button = document.querySelector("#womens_march_button");
+var votingbooth_button = document.querySelector("#votingbooth_button");
+var doctorsoffice_button = document.querySelector("#doctorsoffice_button");
+var track_button = document.querySelector("#track_button");
+
+
 var sup_scene = document.querySelector("#supremecourtscene");
 var seneca = document.querySelector("#seneca");
 
 var womensmarchaudiovar = document.querySelector("#womensmarchaudio");
-var nineteenthamendmentaudio = document.querySelector("#nineteenthamendmentaudio");
+var hillthomasaudio = document.querySelector("#hillthomasaudio");
 var senecafallsaudio = document.querySelector("#senecafallsaudio");
+
+var trackaudio = document.querySelector("#trackaudio");
+var votingboothaudio = document.querySelector("#votingboothaudio");
+var roevwadeaudio = document.querySelector("#roevwadeaudio");
+var doctorsofficeaudio = document.querySelector("#doctorsofficeaudio");
 
 
 //var intro = document.querySelector("#intro");
@@ -32,7 +42,28 @@ sup_scene_button.addEventListener('click', function() {
 womens_march_button.addEventListener('click', function() {
 
   showScene(womens_march.id);
-  playSceneSound(womensmarchaudio.id);
+  playSceneSound(womensmarchaudiovar.id);
+});
+
+track_button.addEventListener('click', function() {
+
+  showScene(track.id);
+  playSceneSound(trackaudio.id);
+
+});
+
+doctorsoffice_button.addEventListener('click', function() {
+
+  showScene(doctorsoffice.id);
+  playSceneSound(doctorsofficeaudio.id);
+
+});
+
+votingbooth_button.addEventListener('click', function() {
+
+  showScene(votingbooth.id);
+  playSceneSound(votingboothaudio.id);
+
 });
 
 
